cd6d6be8b64a0678c85faedded9911d7d1d5b0f9
Vmjkom <vmjkom@utu.fi>
2024-08-27 12:25:40 +0300

    Interactive script change for mahti

--- interactive.sh
+++ interactive.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-salloc -A project_462000353 \
-    -p dev-g \
-    --ntasks-per-node=8 \
-    --gpus-per-node=8 \
-    --cpus-per-task=7 \
-    --mem=480G \
+salloc -A project_2010225 \
+    -p $4 \
+    --ntasks-per-node=4 \
+    --gpus-per-node=a100:4 \
+    --cpus-per-task=32 \
+    --mem=470G \
     --exclusive \
+    --bell \
     -t $2 \
     -N $1 \
     -J $3 \
