ff1c2b94eb7f74f77585812dbe622fa1044d82c9
Ville Komulainen <vmjkom@utu.fi>
2024-11-11 14:05:27 +0200

    .gitignore update Simple bash script for cleaning cache directories

--- slurm_scripts/train.sh
+++ slurm_scripts/train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=neox_1.8B_europa
-#SBATCH --nodes=32
+#SBATCH --job-name=fw-edullama-1.8B
+#SBATCH --nodes=8
 #SBATCH --ntasks-per-node=8
 #SBATCH --cpus-per-task=7
 #SBATCH --mem=480G
 #SBATCH --exclusive
 #SBATCH --partition=standard-g
-#SBATCH --time=02-00:00:00
+#SBATCH --time=12:00:00
 #SBATCH --gpus-per-node=8
 #SBATCH --account=project_462000353
 #SBATCH --output=logs/%x-%j.out
@@ -16,34 +16,30 @@
 ln -f -s $SLURM_JOB_NAME-$SLURM_JOB_ID.out logs/latest.out
 ln -f -s $SLURM_JOB_NAME-$SLURM_JOB_ID.err logs/latest.err
 
-module --force purge
+module purge
 export EBU_USER_PREFIX=/projappl/project_462000353/Easybuild
-module load LUMI/23.09
-module load PyTorch/2.2.2-rocm-5.6.1-python-3.10-singularity-20240617
+module load LUMI partition/G
+module load PyTorch/2.3.0pytorch-2.3.0-rocm-6.2.0-python-3.10
 
+export TRANSFORMERS_CACHE=$HF_HOME #Change this
 
-export TRANSFORMERS_CACHE=$HF_HOME
-
-#DEBUGGING
-export SINGULARITYENV_HIP_LAUNCH_BLOCKING=1
-export SINGULARITYENV_HSA_FORCE_FINE_GRAIN_PCIE=1
-#export SINGULARITYENV_NCCL_DEBUG=INFO
-#export SINGULARITYENV_NCCL_DEBUG_SUBSYS=INIT,COLL
-export SINGULARITYENV_TORCH_DISTRIBUTED_DEBUG=DETAIL
+#LOGGING VERBOSITY
 export SINGULARITYENV_TRANSFORMERS_NO_ADVISORY_WARNINGS=1
 export SINGULARITYENV_TRANSFORMERS_VERBOSITY=error
 
 #Compilers for data builders
-export SINGULARITYENV_CC=gcc-10
-export SINGULARITYENV_CXX=g++-10
+export SINGULARITYENV_CC=gcc-12
+export SINGULARITYENV_CXX=g++-12
 
 #Reduce verbosity in logs
 export SINGULARITYENV_PYTHONWARNINGS=ignore
 export SINGULARITYENV_LANGUAGE="en_US.UTF-8" #Perl complains if these are not set
 export SINGULARITYENV_LC_ALL="en_US.UTF-8"
 
+export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_PORT=9999
 
-CONFIG="configs/ville/125M.yml"
+CONFIG="configs/ablations/llama_2B.yml"
 
 echo "CONF" $CONFIG
 
